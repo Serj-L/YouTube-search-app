e719c06e59aed07c731ed1ae3e876232489d3d5e
Serj-L <svlepniakov@gmail.com>
2021-09-10 17:41:45 +0300

    swith to HashRouter for GH-pages deploy

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom';
 import { Provider as ReduxProvider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import App from './App';
 import { store } from './store';
@@ -9,9 +9,9 @@ import './index.css';
 
 ReactDOM.render(
   <ReduxProvider store={store}>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </ReduxProvider>,
   document.getElementById('root'),
 );

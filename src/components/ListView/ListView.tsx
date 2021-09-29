f885b4f1d463a2298930d75ccb89901f52039d4a
Serj-L <svlepniakov@gmail.com>
2021-09-29 12:38:02 +0300

    fix some styles in ListView

--- src/components/ListView/ListView.tsx
+++ src/components/ListView/ListView.tsx
@@ -20,6 +20,7 @@ const ListView: FC<ListViewProps> = ({ data }) => {
       renderItem={item => (
         <List.Item key={item.videoId}>
           <List.Item.Meta
+            style={{ height: 88 }}
             avatar={
               <Typography.Link
                 href={`https://www.youtube.com/watch?v=${item.videoId}`}
@@ -37,7 +38,7 @@ const ListView: FC<ListViewProps> = ({ data }) => {
             title={
               <Typography.Paragraph
                 ellipsis={{ rows: 2, expandable: false }}
-                style={{ marginBottom: 8 }}
+                style={{ margin: 0 }}
               >
                 <Typography.Link
                   className={styles.videoLink}
@@ -49,9 +50,13 @@ const ListView: FC<ListViewProps> = ({ data }) => {
               </Typography.Paragraph>
             }
             description={
-              <Typography.Text
+              <Typography.Paragraph
                 className={styles.viewsCount}
-                style={{ color: 'rgba(23, 23, 25, 0.3)' }}
+                style={{
+                  color: 'rgba(23, 23, 25, 0.3)',
+                  margin: 0,
+                }}
+                ellipsis={{ rows: 1, expandable: false }}
               >
                 <Typography.Link
                   className={styles.channelLink}
@@ -66,7 +71,8 @@ const ListView: FC<ListViewProps> = ({ data }) => {
                   Number(item.viewCount) / 1000 >= 1 ?
                     `${numFormatter.format(Math.round(Number(item.viewCount) / 1000))} тыс. просмотров` :
                     `${item.viewCount} ${declOfNum(Number(item.viewCount),['просмотр', 'просмотра', 'просмотров'])}`}
-              </Typography.Text> }
+              </Typography.Paragraph>
+            }
           />
         </List.Item>
       )}
